287f82aa0bbbd00143139da62a05a7e43f780cf0
umadkek <dharamsohanpal@gmail.com>
2024-04-24 22:55:28 -0600

    removed class files cus why not, added vertex, triangle, and matrix3 class, which lets me create its respective object except for matrix3, which does math

--- renderer/DemoViewer.java
+++ renderer/DemoViewer.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
 
 public class DemoViewer {
     public static void main(String[] args) {
-        JFrame f = new JFrame();
-        Container pane = f.getContentPane();
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
 
         // slider to control horizontal rotation
-        JSlider horizontalSlider = new JSlider(0, 360, 180);
+        JSlider horizontalSlider = new JSlider(-180, 180, 0);
         pane.add(horizontalSlider, BorderLayout.SOUTH);
 
         // slider to control vertical rotation
@@ -17,17 +19,98 @@ public class DemoViewer {
 
         // panel to show render resuults
         JPanel renderPanel = new JPanel() {
-            public void paintComponents(Graphics g) {
+            public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
                 // rendering magic
-            }
-        };
+                ArrayList<Triangle> tris = new ArrayList<Triangle>();
+                tris.add(new Triangle(new Vertex(100, 100, 100), 
+                                    new Vertex(-100, -100, 100), 
+                                    new Vertex(-100, 100, -100),
+                                    Color.WHITE));
+
+                tris.add(new Triangle(new Vertex(100, 100, 100), 
+                                    new Vertex(-100, -100, 100), 
+                                    new Vertex(-100, 100, -100),
+                                    Color.RED));
+
+                tris.add(new Triangle(new Vertex(-100, 100, -100), 
+                                    new Vertex(100, -100, -100), 
+                                    new Vertex(100, 100, 100),
+                                    Color.GREEN));
+                
+                tris.add(new Triangle(new Vertex(-100, 100, -100), 
+                                    new Vertex(100, -100, -100), 
+                                    new Vertex(-100, -100, 100),
+                                    Color.BLUE));
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for (Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
+            }};
         pane.add(renderPanel, BorderLayout.CENTER);
-        f.setSize(400, 400);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
+
+class Vertex {
+    double x;
+    double y;
+    double z;
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
+
+class Matrix3 {
+    double[] values;
+    Matrix3(double[] values) {
+        this.values = values;
+    }
+
+    Matrix3 multiply(Matrix3 other) {
+        double[] result = new double[0];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                for (int i = 0; i < 3; i++) {
+                    result[row * 3 + col] +=
+                        this.values[row * 3 + i] * other.values[i * 3 + col];
+                }
+            }
+        }
+        return new Matrix3(result);
+    }
+
+    Vertex transform(Vertex in) {
+        return new Vertex(
+            in.x * values[0] + in.y * values[3] + in.z * values[6],
+            in.x * values[1] + in.y * values[4] + in.z * values[7],
+            in.x * values[2] + in.y * values[5] + in.z * values[8]
+        );
     }
 }
